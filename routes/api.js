d9874b7d5be3e1142a0365c407a6d17fc81a3d2e
Rahul Sharma <rahulsharma@Sunitas-MacBook-Pro.local>
2018-08-16 22:27:25 -0400

    api fix

--- routes/api.js
+++ routes/api.js
@@ -4,9 +4,10 @@ var router = express.Router();
 /* GET users listing. */
 router.post('/', function(req, res, next) {
 	let speech = 
-		req.body.result &&
-		req.body.result.parameters &&
-		req.body.result.parameters.isName
+		req.body.result 
+		// req.body.result.parameters &&
+		// req.body.result.parameters.isName
+			// ? req.body.result.parameters.isName
 			? req.body.result.parameters.isName
 			: "Didnt matter";
 
@@ -14,8 +15,8 @@ router.post('/', function(req, res, next) {
 		
 		return res.json({
 			req1: req.body.result,
-			req2: req.body.result.parameters,
-			req3: req.body.result.parameters.isName,
+			// req2: req.body.result.parameters,
+			// req3: req.body.result.parameters.isName,
 			speech: speech,
 			displayText: speech,
 			source: 'webhook-isAuthorized-sample'
